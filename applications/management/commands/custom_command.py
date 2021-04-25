abd1eed32774d96fe0a1671cd275e2b2f13ca229
butaneko <butaneko@buta.neko>
2021-04-25 17:37:58 +0900

    commited

--- applications/management/commands/custom_command.py
+++ applications/management/commands/custom_command.py
@@ -234,27 +234,30 @@ def boot_selenium():
 	chrome_options.add_argument('--disable-gpu')
 	chrome_options.add_argument('--no-sandbox')
 	chrome_options.add_argument('--disable-dev-shm-usage')
-	proxy_host='140.227.171.218'
-	proxy_port='3128'
-	chrome_options.add_argument(f"--proxy-server=http://{proxy_host}:{proxy_port}")
-	# """
-	# # アダプタエラー、自動テスト…、を非表示
-	# chrome_options.add_experimental_option("excludeSwitches",['enable-automation',
-	# 																													'enable-logging'])
-	# chrome_options.add_argument('--headless')  #ヘッドレスモード
-	# chrome_options.add_argument('--incognito')  #シークレットモード
-	# chrome_options.add_argument('--disable-gpu')
-	# chrome_options.add_argument('--disable-desktop-notifications')
-	# chrome_options.add_argument("--disable-extensions")
-	# chrome_options.add_argument('--disable-dev-shm-usage') #/dev/shmを使わないように指定
-	# chrome_options.add_argument('--disable-application-cache')
-	# chrome_options.add_argument('--no-sandbox')
-	# chrome_options.add_argument('--single-process')
+	# proxy_host='140.227.171.218'
+	# proxy_port='3128'
+	# chrome_options.add_argument(f"--proxy-server=http://{proxy_host}:{proxy_port}")
 	# chrome_options.add_argument('--ignore-certificate-errors')
-	# chrome_options.add_argument('--user-agent=aheahe')
-	# chrome_options.add_argument('--blink-settings=imagesEnabled=false') #画像を非表示
-	# chrome_options.page_load_strategy='none' #
-	# """
+	# chrome_options.add_argument('--ignore-ssl-errors')
+	chrome_options.add_experimental_option("prefs",{"profile.managed_default_content_settings.javascript":2})
+	"""
+	# アダプタエラー、自動テスト…、を非表示
+	chrome_options.add_experimental_option("excludeSwitches",['enable-automation',
+																														'enable-logging'])
+	chrome_options.add_argument('--headless')  #ヘッドレスモード
+	chrome_options.add_argument('--incognito')  #シークレットモード
+	chrome_options.add_argument('--disable-gpu')
+	chrome_options.add_argument('--disable-desktop-notifications')
+	chrome_options.add_argument("--disable-extensions")
+	chrome_options.add_argument('--disable-dev-shm-usage') #/dev/shmを使わないように指定
+	chrome_options.add_argument('--disable-application-cache')
+	chrome_options.add_argument('--no-sandbox')
+	chrome_options.add_argument('--single-process')
+	chrome_options.add_argument('--ignore-certificate-errors')
+	chrome_options.add_argument('--user-agent=aheahe')
+	chrome_options.add_argument('--blink-settings=imagesEnabled=false') #画像を非表示
+	chrome_options.page_load_strategy='none' #
+	"""
 	"""
 	chrome_options.add_argument('--headless')
 	chrome_options.add_argument('--disable-gpu')
