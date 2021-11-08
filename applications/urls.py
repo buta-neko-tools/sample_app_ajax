a70312db46dbc8918b166b01fb7cf75912e84f99
butaneko <butaneko@buta.neko>
2021-11-08 13:25:14 +0900

    commit

--- applications/urls.py
+++ applications/urls.py
@@ -8,14 +8,10 @@ urlpatterns = [
 	# path('userdata/',views.userdata,name='userdata'),
 	# path('dballdata/',views.dballdata,name='dballdata'),
 	# path('test01/',views.test01,name='test01'),
-
 	path('index/',views.index,name='index'),
 	path('ajax-number/',views.ajax_number,name='ajax_number'),
 	path('dballdata/',views.dballdata,name='dballdata'),
-
-	path('mqtt_test01/',views.mqtt_test01,name='mqtt_test01'),
-	path('mqtt_ajax/',views.mqtt_ajax,name='mqtt_ajax'),
-
+	# path('mqtt_test01/',views.mqtt_test01,name='mqtt_test01'),
+	# path('mqtt_ajax/',views.mqtt_ajax,name='mqtt_ajax'),
 	path('mqtt_test02/',views.mqtt_test02,name='mqtt_test02'),
-
 ]
